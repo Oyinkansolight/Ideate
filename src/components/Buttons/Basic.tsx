ab590887812554e38a37b6605cb45e55737c015e
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2022-02-23 12:19:53 +0100

    update button component to receive click function

--- src/components/Buttons/Basic.tsx
+++ src/components/Buttons/Basic.tsx
@@ -3,10 +3,12 @@ import React from "react";
 interface Props {
 	text: string;
 	type?: string;
+	onPress?: (any) => void;
 }
 
 const BasicButton = (props: Props) => (
 	<button
+	onClick={props.onPress}
 		className={`px-10 py-3 font-semibold transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 ${
 			props.type === "light"
 				? "text-chathams bg-gray-300 hover:bg-gray-200"
