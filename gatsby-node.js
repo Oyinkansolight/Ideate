9e005b33364e1a4b3a76a8e828da2ad216174e77
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2022-03-06 08:59:30 +0100

    fix

---
+++ gatsby-node.js
@@ -0,0 +1,19 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    
+    actions.setWebpackConfig({
+      node: {
+        fs: 'empty'
+      }
+    })
+  ​
+    if(stage === 'build-html'){
+      actions.setWebpackConfig({
+        module: {
+          rules: [{
+            test: /canvas/,
+            use: loaders.null()
+          }]
+        }
+      })
+    }
+  }
