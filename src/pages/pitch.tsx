b9a39ded1c6fa8f3d801f881d8697b771d3bcf25
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2022-03-07 16:48:30 +0100

    init typeform

--- src/pages/pitch.tsx
+++ src/pages/pitch.tsx
@@ -2,50 +2,59 @@ import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import { BasicButton } from "../components/Buttons";
+import { Widget } from "@typeform/embed-react";
 
-const Pitch = () => {
+// const Pitch = () => {
+// 	return (
+// 		<div className='h-screen w-screen'>
+// 			<StaticImage
+// 				src='../images/pitch.png'
+// 				alt='Apply'
+// 				className='fixed z-0 h-full w-full'
+// 			/>
+
+// 			<div className='bg-chathams fixed h-full w-full opacity-80'></div>
+
+// 			<div className='absolute flex w-full'>
+// 				<div className=' z-10 my-52 mx-auto h-[500px] w-2/5 overflow-hidden bg-gray-300 shadow-md'>
+// 					<div className='mx-auto flex flex-col gap-y-6 px-36 py-10'>
+// 						<div>
+// 							<StaticImage
+// 								src='../images/ideate.png'
+// 								alt='Logo'
+// 								width={300}
+// 							/>
+// 						</div>
+
+// 						<div className='text-lg'>
+// 							We provide exceptional startups with capital, expert
+// 							operational support and access to world-class
+// 							corporates.
+// 						</div>
+
+// 						<div className='text-chathams mt-10 text-2xl font-semibold'>
+// 							Submit pitch deck: Tell us about your business or
+// 							idea.
+// 						</div>
+
+// 						<div>
+// 							<Link to='/pitch-form'>
+// 								<BasicButton text='Begin Application' />
+// 							</Link>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default Pitch;
+
+const MyComponent = () => {
 	return (
-		<div className='h-screen w-screen'>
-			<StaticImage
-				src='../images/pitch.png'
-				alt='Apply'
-				className='fixed z-0 h-full w-full'
-			/>
-
-			<div className='bg-chathams fixed h-full w-full opacity-80'></div>
-
-			<div className='absolute flex w-full'>
-				<div className=' z-10 my-52 mx-auto h-[500px] w-2/5 overflow-hidden bg-gray-300 shadow-md'>
-					<div className='mx-auto flex flex-col gap-y-6 px-36 py-10'>
-						<div>
-							<StaticImage
-								src='../images/ideate.png'
-								alt='Logo'
-								width={300}
-							/>
-						</div>
-
-						<div className='text-lg'>
-							We provide exceptional startups with capital, expert
-							operational support and access to world-class
-							corporates.
-						</div>
-
-						<div className='text-chathams mt-10 text-2xl font-semibold'>
-							Submit pitch deck: Tell us about your business or
-							idea.
-						</div>
-
-						<div>
-							<Link to='/pitch-form'>
-								<BasicButton text='Begin Application' />
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<Widget id='hx7iL825' style={{ width: "100%", height: "100vh" }} className='my-form' />
 	);
 };
 
-export default Pitch;
+export default MyComponent;
