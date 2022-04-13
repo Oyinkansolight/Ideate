c92381e66ead344e9ff5c13d2f8fb3772dbc7ff5
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2022-04-13 11:12:11 +0100

    Under Maintenance

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function index() {
+	return (
+		<div className='flex h-screen w-screen'>
+			<div className='m-auto text-center text-7xl font-bold'>
+				Website Under Maintenance
+			</div>
+		</div>
+	);
+}
