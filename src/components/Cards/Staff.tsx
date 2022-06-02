75c3b4b6bcfa027eaff7bfe405855370e92987b3
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2022-06-02 15:48:48 +0100

    update name

--- src/components/Cards/Staff.tsx
+++ src/components/Cards/Staff.tsx
@@ -69,7 +69,7 @@ export default function StaffCard() {
 						ducimus aperiam.
 					</div>
 
-                    <div className="font-semibold text-2xl text-chathams">Patrick Inyangetoh</div>
+                    <div className="font-semibold text-2xl text-chathams">Patrick Oke</div>
 
                     <div className="text-xs font-light text-chathams-400">Founder</div>
 				</div>
